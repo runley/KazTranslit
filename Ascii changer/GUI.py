6f9a8ddf5bf9b00f77275b07f3a323b9cc34e7e9
Runley <runley101@gmail.com>
2020-02-18 20:54:11 +0000

    changed everything

---
+++ Ascii changer/GUI.py
@@ -0,0 +1,57 @@
+from tkinter import *
+from tkinter import ttk
+import functions as fn
+from subprocess import call
+# Functions but most of other funcs are in a methods.py in the same folder.
+
+class Main:  # Main class.
+    def __init__(self):
+        root = self.root = Tk()  # Init for main window.
+        root.title("KTL - Kaz TransLit")
+        root.configure(background="#f16161")
+
+        # Row 0
+        # Photo
+        logo = PhotoImage(file="images/TK_logo.png")
+        lbl = Label(root, image=logo, bg="#f16161").grid(row=0, column=0, pady=5, sticky=W)
+
+        btnframe = Frame(root, bg="#f16161") #btn frame
+        btnframe.grid(row=0, column=1) # Toolbar on top of the window.
+        faq_btn = Button(btnframe, text="FAQ", command=fn.faqKTL, font="none 12",
+                         bg="#df9999", width=10, height=1)
+        faq_btn.grid(row=0, column=0, pady=5, padx = 10, sticky=NW)
+        man_btn = Button(btnframe, text="Manual", command=fn.manualKTL, font="none 12",
+                         bg="#df9999", width=10, height=1)
+        man_btn.grid(row=0, column=1, pady=5, padx = 10, sticky=NW)
+        cnt_btn = Button(btnframe, text="Contant Me",
+                         command=fn.contactKTL, font="none 12", bg="#df9999",
+                         width=10, height=1)
+        cnt_btn.grid(row=0, column=2, pady=5, padx = 10, sticky=NW)
+        # Row 1
+        logotxt = Label(root, text="KazTransLit", bg="#f16161", fg="white",
+                    font="none 12 bold")
+        logotxt.grid(row=1, column=0, padx=10, pady=5, sticky=W)
+        btn = Button(root, text="Document to Text", command=fn.documentKTL,
+                     font="none 12", bg="#df9999", width=20, height=5)
+        btn.grid(row=1, column=1, padx=10, pady=5, sticky=E)
+        btn = Button(root, text="Real Time Transliteration", command=fn.realtimeKTL,
+                     font="none 12", bg="#df9999", width=20, height=5)
+        btn.grid(row=1, column=2, padx=10, pady=5, sticky=E)
+        # Row 2
+        btn = Button(root, text="Settings", command = fn.settingsKTL, font="none 12", bg="#df9999", width=20,
+                     height=5)
+        btn.grid(row=2, column=2, padx=10, pady=5, sticky=E)
+        # Exit
+        btn = Button(root, text="EXIT", font="none 10", bg="#FF4C4C", width=20, height=5,
+                     command=self.close_root)
+        btn.grid(row=10, column=0, padx=10, pady=5, sticky=W)
+
+        root.resizable(1, 1)
+        root.iconbitmap('images/TK_logo.ico')
+        root.mainloop()
+
+    def close_root(self):  # Kill func.
+        self.root.destroy()
+        exit()
+
+Main()
--- Ascii changer/functions.py
+++ Ascii changer/functions.py
@@ -30,19 +30,74 @@ def bufferCleaner(BufferLocation): #cleans buffer file
   tempBuffer = open(BufferLocation,"w")
   tempBuffer.close()
 
+
+
 #---------------
 #window functions
 #---------------
-def documentTKL(): #top level func to create a new window as Toplevel
-    docTKL = Toplevel(bg = "#f16161")
-    docTKL.title('Document TKL')
-    docTKL.wm_geometry("794x370")
-    docTKL.iconbitmap('Ascii changer\images\TK_logo.ico')
-    #insert labels and stuff
+def documentKTL(): #top level func to create a new window as Toplevel
+    docKTL = Toplevel(bg = "#f16161")
+    docKTL.title('Document to Text - KTL')
+    logotxt = Label(docKTL, text="TransKazLit", bg="#f16161", fg="white",font="none 24 bold")
 
-def realtimeTKL(): #top level func to create a new window as Toplevel
-    docTKL = Toplevel(bg = "#f16161")
-    docTKL.title('Real-Time TKL')
-    docTKL.wm_geometry("794x370")
-    docTKL.iconbitmap('Ascii changer\images\TK_logo.ico')
-    #insert labels and stuff
+    docKTL.iconbitmap('images/TK_logo.ico')
+
+
+def realtimeKTL():
+    realKTL = Toplevel(bg = "#f16161")
+    realKTL.title('Real-Time - KTL')
+    # Row 0
+    # Photo
+    logotxt = Label(realKTL, text="TransKazLit", bg="#f16161", fg="white",font="none 24 bold")
+    logotxt.grid(row=1, column=0, padx=10, pady=5, sticky=W)
+
+    realKTL.iconbitmap('images/TK_logo.ico')
+
+
+
+def manualKTL():
+    manKTL = Toplevel(bg = "#f16161")
+    manKTL.title('Manual - KTL')
+    #row 0
+    logotxt = Label(manKTL, text="TransKazLit", bg="#f16161", fg="white",font="none 24 bold")
+    logotxt.grid(row=0, column=0, padx=10, pady=5, sticky=W)
+    #row 1
+    lbl = Label(manKTL, text="This is the user manual.", bg="#f16161", fg="white",font="none 14 bold")
+    lbl.grid(row=1, column=0, padx=10, pady=5, sticky=W)
+    lbl = Label(manKTL, )
+    #misc
+    manKTL.iconbitmap('images/TK_logo.ico')
+
+def faqKTL():
+    faqKTL = Toplevel(bg = "#f16161")
+    faqKTL.title('Manual - KTL')
+    #misc
+    faqKTL.iconbitmap('images/TK_logo.ico')
+
+def contactKTL():
+    contactKTL = Toplevel(bg = "#f16161")
+    contactKTL.title('Manual - KTL')
+    #row 0
+    logotxt = Label(contactKTL, text="TransKazLit", bg="#f16161", fg="white",font="none 24 bold")
+    logotxt.grid(row=0, column=0, padx=10, pady=5, sticky=W)
+    #row 1
+    lbl = Label(contactKTL, text="Contact me using the following methods:", bg="#f16161", fg="white",font="none 14 bold")
+    lbl.grid(row=1, column=0, padx=10, pady=5, sticky=W)
+    #row 2
+    lbl = Label(contactKTL, text="Email: sultanbekov.02@outlook.com", bg="#f16161", fg="white",font="none 14")
+    lbl.grid(row=2, column=0, padx=10, pady=5, sticky=W)
+    #row  3
+    lbl = Label(contactKTL, text="Instagram: @_runley", bg="#f16161", fg="white",font="none 14")
+    lbl.grid(row=3, column=0, padx=10, pady=5, sticky=W)
+    # Exit
+    btn = Button(contactKTL, text="EXIT", font="none 10", bg="#FF4C4C", width=10, height=3)
+    btn.grid(row=10, column=0, padx=10, pady=5, sticky=W)
+
+    #misc
+    contactKTL.iconbitmap('images/TK_logo.ico')
+
+def settingsKTL():
+    settingsKTL = Toplevel(bg = "#f16161")
+    settingsKTL.title('Settings - KTL')
+    #misc
+    settingsKTL.iconbitmap('images/TK_logo.ico')
