ba3f63b2a29ff8d84b3860eaefb7cef9dc980dc6
Runley <45308862+runley@users.noreply.github.com>
2020-03-02 16:58:46 +0000

    Update GUI.py

--- Ascii changer/GUI.py
+++ Ascii changer/GUI.py
@@ -1,186 +1,59 @@
 from tkinter import *
 from tkinter import ttk
+import functions as fn
+from subprocess import call
+# Functions but most of other funcs are in a methods.py in the same folder.
 
-#dictionary
-kazalphabet = {"А":"A","Ә":"Ä","Б":"B","В":"V","Г":"G","Ғ":"Ğ","Д":"D","Е":"E","Ё":"YO","Ж":"J","З":"Z","И":"Ï","Й":"Y","К":"K","Қ":"Q","Л":"L","М":"M","Н":"N","Ң":"Ñ","О":"O","Ө":"Ö","П":"P","Р":"R","С":"S","Т":"T","У":"W","Ұ":"U","Ү":"Ü","Ф":"F","Х":"X","Һ":"H","Ц":"C","Ч":"Ç","Ш":"Ş","Щ":"ŞÇ","Ъ":"\"","Ы":"I","І":"I","Ь":"\'","Э":"É","Ю":"YU","Я":"YA","а":"a","ә":"ä","б":"b","в":"v","г":"g","ғ":"ğ","д":"d","е":"e","ё":"yo","ж":"j","з":"z","и":"ï","й":"y","к":"k","л":"l","м":"m","н":"n","ң":"ñ","о":"o","ө":"ö","п":"p","р":"r","с":"s","т":"t","у":"w","ұ":"u","ү":"ü","ф":"f","х":"x","һ":"h","ц":"c","ч":"ç","ш":"ş","щ":"şç","ъ":"\"","ы":"ı","і":"i","ь":"\'","э":"é","ю":"yu","я":"ya"}
+class Main:  # Main class.
+    def __init__(self):
+        root = self.root = Tk()  # Init for main window.
+        root.title(" - Kaz TransLit")
+        root.configure(background="#f16161")
 
-#-------------------
-#functions
-#-------------------
-def stringChanger(stringinp,Buffer,alphabetDic): #magic code, works, dont touch
-  bufferFile = open(Buffer,"a+")
-  teststring = stringinp
-  for i in alphabetDic:
-    x = alphabetDic.get(i)
-    teststring = teststring.replace(i,x)
-  bufferFile.write(teststring)
+        # Row 0
+        # Photo
+        logo = PhotoImage(file="images/KTL_logo.png")
+        lbl = Label(root, image=logo, bg="#f16161").grid(row=0, column=0, pady=5, padx =0, sticky=NW)
+        #btn frame
+        btnframe = Frame(root, bg="#f16161")
+        btnframe.grid(row=0, column=1,pady = 5, sticky=N) # Toolbar on top of the window.
+        faq_btn = Button(btnframe, text="FAQ", command=fn.faqKTL, font="none 12",bg="#EF4A4A", width=10, height=1)
+        faq_btn.grid(row=0, column=0, pady=5, padx = 10, sticky=NW)
+        man_btn = Button(btnframe, text="Manual", command=fn.manualKTL, font="none 12",bg="#EF4A4A", width=10, height=1)
+        man_btn.grid(row=0, column=1, pady=5, padx = 10, sticky=NW)
+        cnt_btn = Button(btnframe, text="Contant Me", command=fn.contactKTL, font="none 12", bg="#EF4A4A", width=10, height=1)
+        cnt_btn.grid(row=0, column=2, pady=5, padx = 10, sticky=NW)
 
+        # Row 1
+        lbl = Label(root, text="KazTransLit", bg="#f16161", fg="white", font="Bahnschrift 12 bold")
+        lbl.grid(row=1, column=0, padx=10, pady=5, sticky=SW)
+        btn = Button(root, text="Document to Text", command=fn.documentKTL, font="none 12", bg="#EF4A4A", width=20, height=5)
+        btn.grid(row=1, column=1, padx=10, pady=5, sticky=E)
+        btn = Button(root, text="Real Time Transliteration", command=fn.realtimeKTL, font="none 12", bg="#EF4A4A", width=20, height=5)
+        btn.grid(row=1, column=2, padx=10, pady=5, sticky=E)
 
-def callLoop(stringinp,Buffer,alphabetDic): #calls itself every 10 seconds
-    proc_text = stringChanger(stringinp, Buffer, alphabetDic)
-    tk.after(10, lambda: callLoop(stringinp,Buffer,alphabetDic))
+        # Row 2
+        btn = Button(root, text="Settings", command = fn.settingsKTL, font="none 12", bg="#EF4A4A", width=20, height=5)
+        btn.grid(row=2, column=2, padx=10, pady=5, sticky=E)
+        lbl = Label(root, text="Transliteration on a whole 'nother level", bg="#f16161", fg="white", font="Bahnschrift 12")
+        lbl.grid(row=2, column=0, padx=10, pady=5, sticky=N)
+        # Exit
+        btn = Button(root, text="EXIT", font="none 10", bg="#FF4C4C", width=20, height=5, command=self.close_root)
+        btn.grid(row=10, column=0, padx=10, pady=5, sticky=W)
 
+        root.resizable(0, 0)
+        root.iconbitmap('images/KTL_logo.ico')
+        root.mainloop()
 
-def userInput(): #gets user input for further proccessing
-  inputvar = input("enter filename:\n>")
-  return inputvar
+    def close_root(self):  # Kill func.
+        self.root.destroy()
+        exit()
 
+Main()
 
-def askUser(userInput): #takes in userinput to convert to file object, returns file
-  try:
-    userFile = open(userInput,"r+") #tries to convert and find file
-    return userFile
-  except:
-    print("error occured in functions.askUser()") #does not find file
+#colors
+#EF4A4A 5% shade for f16161
+#f16161 main color light red
+#FF4C4C darker shade of f16161 for exit buttons
 
-
-def bufferCleaner(BufferLocation): #cleans buffer file
-  tempBuffer = open(BufferLocation,"w")
-  tempBuffer.close()
-
-
-def exit_btn(name): # closes top levels by taking the name of the top level
-        name.destroy()
-        name.update()
-#---------------
-#window functions
-#---------------
-def documentKTL(): #top level func to create a new window as Toplevel
-    docKTL = Toplevel(bg = "#f16161")
-    docKTL.title('Document to Text - KTL')
-
-    #row 0
-    logotxt = Label(docKTL, text="TransKazLit", bg="#f16161", fg="white",font="Bahnschrift 24 bold")
-    logotxt.grid(row=0, column=0, padx=10, pady=5, sticky=W)
-
-    #row 1
-    lbl = Label(docKTL, text= "wololo", bg="#f16161", fg="white",font="Bahnschrift 14 bold")
-    lbl.grid(row=1, column=0, pady=5, sticky=W)
-
-    #row 2
-
-    #close window
-    btn = Button(docKTL, command = lambda: exit_btn(docKTL), text="close window", font="none 10", bg="#FF4C4C", width=10, height=3)
-    btn.grid(row=10, column=0, padx=10, pady=5, sticky=W)
-
-    #misc
-    docKTL.iconbitmap('images/KTL_logo.ico')
-    docKTL.resizable(0, 0)
-
-
-def realtimeKTL():
-    realKTL = Toplevel(bg = "#f16161")
-    realKTL.title(' - Real-time')
-
-    # Row 0
-    logotxt = Label(realKTL, text="TransKazLit", bg="#f16161", fg="white", font="Bahnschrift 24 bold")
-    logotxt.grid(row=0, column=0, padx=10, pady=5, sticky=W)
-
-    #row 1
-    lbl = Label(realKTL,bg="#f16161", text = "Enter text here:", fg = "white", font="Bahnschrift 12")
-    lbl.grid(row = 1, column= 0,)
-    lbl = Label(realKTL,bg="#f16161", text = "Watch magic happen here:", fg = "white", font="Bahnschrift 12")
-    lbl.grid(row = 1, column= 1,)
-    #row 2
-    txtbox = Text(realKTL, height = 20, width = 50)
-    txtbox.grid(row=2, column=0, pady = 5, padx = 5)
-    outbox = Text(realKTL, height = 20, width = 50)
-    outbox.grid(row=2, column=1, pady = 5, padx = 5)
-
-    #row 3
-
-    #close top
-    btn = Button(realKTL, command = lambda: exit_btn(realKTL), text="close window", font="none 10", bg="#FF4C4C", width=10, height=3)
-    btn.grid(row=10, column=0, padx=10, pady=5, sticky=W)
-
-    #misc
-    #if txtbox.get(0.0, END) == !"null":
-    #    global textbox = textbox = txtbox.get(0.0, END)
-    #    callLoop(textbox, bufferedfile.txt, kazalphabet)
-
-    realKTL.iconbitmap('images/KTL_logo.ico')
-    realKTL.resizable(0, 0)
-
-
-def settingsKTL():
-    settingsKTL = Toplevel(bg = "#f16161")
-    settingsKTL.title(' - Settings')
-
-    #row 0
-    logotxt = Label(settingsKTL, text="KazTranslit", bg="#f16161", fg="white",font="Bahnschrift 24 bold")
-    logotxt.grid(row=0, column=0, padx=10, pady=5, sticky=W)
-
-    #close top
-    btn = Button(settingsKTL, command = lambda: exit_btn(settingsKTL), text="close window", font="none 10", bg="#FF4C4C", width=10, height=3)
-    btn.grid(row=10, column=0, padx=10, pady=5, sticky=W)
-
-    #misc
-    settingsKTL.iconbitmap('images/KTL_logo.ico')
-    settingsKTL.resizable(0, 0)
-
-
-def manualKTL():
-    manKTL = Toplevel(bg = "#f16161")
-    manKTL.title(' - Manual')
-
-    #row 0
-    logotxt = Label(manKTL, text="KazTranslit", bg="#f16161", fg="white",font="Bahnschrift 24 bold")
-    logotxt.grid(row=0, column=0, padx=10, pady=5, sticky=W)
-
-    #row 1
-    lbl = Label(manKTL, text="This is the user manual.", bg="#f16161", fg="white",font="none 14 bold")
-    lbl.grid(row=1, column=0, padx=10, pady=5, sticky=W)
-
-    #close top
-    btn = Button(manKTL, command = lambda: exit_btn(manKTL), text="close window", font="none 10", bg="#FF4C4C", width=10, height=3)
-    btn.grid(row=10, column=0, padx=10, pady=5, sticky=W)
-
-    #misc
-    manKTL.iconbitmap('images/KTL_logo.ico')
-    manKTL.resizable(0, 0)
-
-
-def faqKTL():
-    faqKTL = Toplevel(bg = "#f16161")
-    faqKTL.title(' - FAQ')
-
-    #row 0
-    logotxt = Label(faqKTL, text="KazTranslit", bg="#f16161", fg="white",font="Bahnschrift 24 bold")
-    logotxt.grid(row=0, column=0, padx=10, pady=5, sticky=W)
-
-    #close top
-    btn = Button(faqKTL, command = lambda: exit_btn(faqKTL), text="close window", font="none 10", bg="#FF4C4C", width=10, height=3)
-    btn.grid(row=10, column=0, padx=10, pady=5, sticky=W)
-
-    #misc
-    faqKTL.iconbitmap('images/KTL_logo.ico')
-    faqKTL.resizable(0, 0)
-
-
-def contactKTL():
-    contactKTL = Toplevel(bg = "#f16161")
-    contactKTL.title(' - Contact')
-
-    #row 0
-    logotxt = Label(contactKTL, text="KazTranslit", bg="#f16161", fg="white",font="Bahnschrift 24 bold")
-    logotxt.grid(row=0, column=0, padx=10, pady=5, sticky=W)
-
-    #row 1
-    lbl = Label(contactKTL, text="Contact me using the following methods:", bg="#f16161", fg="white",font="Bahnschrift 14 bold")
-    lbl.grid(row=1, column=0, padx=10, pady=5, sticky=W)
-
-    #row 2
-    lbl = Label(contactKTL, text="Email: sultanbekov.02@outlook.com", bg="#f16161", fg="white",font="Bahnschrift 14")
-    lbl.grid(row=2, column=0, padx=10, pady=5, sticky=W)
-
-    #row  3
-    lbl = Label(contactKTL, text="Instagram: @_runley", bg="#f16161", fg="white",font="Bahnschrift 14")
-    lbl.grid(row=3, column=0, padx=10, pady=5, sticky=W)
-
-    #close top
-    btn = Button(contactKTL, command = lambda: exit_btn(contactKTL), text="close window", font="none 10", bg="#FF4C4C", width=10, height=3)
-    btn.grid(row=10, column=0, padx=10, pady=5, sticky=W)
-
-    #misc
-    contactKTL.iconbitmap('images/KTL_logo.ico')
-    contactKTL.resizable(0, 0)
+#kazalphabet = {"А":"A","Ә":"Ä","Б":"B","В":"V","Г":"G","Ғ":"Ğ","Д":"D","Е":"E","Ё":"YO","Ж":"J","З":"Z","И":"Ï","Й":"Y","К":"K","Қ":"Q","Л":"L","М":"M","Н":"N","Ң":"Ñ","О":"O","Ө":"Ö","П":"P","Р":"R","С":"S","Т":"T","У":"W","Ұ":"U","Ү":"Ü","Ф":"F","Х":"X","Һ":"H","Ц":"C","Ч":"Ç","Ш":"Ş","Щ":"ŞÇ","Ъ":"\"","Ы":"I","І":"I","Ь":"\'","Э":"É","Ю":"YU","Я":"YA","а":"a","ә":"ä","б":"b","в":"v","г":"g","ғ":"ğ","д":"d","е":"e","ё":"yo","ж":"j","з":"z","и":"ï","й":"y","к":"k","л":"l","м":"m","н":"n","ң":"ñ","о":"o","ө":"ö","п":"p","р":"r","с":"s","т":"t","у":"w","ұ":"u","ү":"ü","ф":"f","х":"x","һ":"h","ц":"c","ч":"ç","ш":"ş","щ":"şç","ъ":"\"","ы":"ı","і":"i","ь":"\'","э":"é","ю":"yu","я":"ya"}
