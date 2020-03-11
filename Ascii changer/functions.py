62f66181b9366269967049274e236bc189d3bd72
Runley <45308862+runley@users.noreply.github.com>
2020-03-11 09:29:42 +0000

    realtime now works fine
    bit untidy code but it works

--- Ascii changer/functions.py
+++ Ascii changer/functions.py
@@ -25,11 +25,6 @@ def realtimeChanger(stringinp,alphabetDic): #magic code, works, dont touch
       procingstring = procingstring.replace(i,x)
     return procingstring
 
-def callLoop(self,stringinp,alphabetDic): #calls itself every 10 seconds
-    proc_text = realtimeChanger(stringinp,alphabetDic)
-    if proc_text != "":
-        return proc_text
-    #realKTL.after(10, callLoop(textbox, "bufferedfile.txt", kazalphabet))
 
 def userInput(): #gets user input for further proccessing
     inputvar = input("enter filename:\n>")
@@ -41,7 +36,7 @@ def askUser(userInput): #takes in userinput to convert to file object, returns f
         userFile = open(userInput,"r+") #tries to convert and find file
         return userFile
     except:
-        print("error occured in functions.askUser()") #does not find file
+        print("error occured in functions.askUser()")#does not find file
 
 
 def bufferCleaner(BufferLocation): #cleans buffer file
@@ -78,7 +73,8 @@ def documentKTL(): #top level func to create a new window as Toplevel
     docKTL.resizable(0, 0)
 
 
-def realtimeKTL():
+def realtimeKTL(): #top level for realtime window + code to make the window work
+    global txtboxstring
     realKTL = Toplevel(bg = "#f16161")
     realKTL.title(' - Real-time')
     # Row 0
@@ -95,26 +91,24 @@ def realtimeKTL():
     outbox = Text(realKTL, height = 20, width = 50)
     outbox.grid(row=2, column=1, pady = 5, padx = 5)
     textbox = txtbox.get(0.0, END)
-    #row 3
     #close top
     btn = Button(realKTL, command = lambda: exit_btn(realKTL), text="close window", font="none 10", bg="#FF4C4C", width=10, height=3)
     btn.grid(row=10, column=0, padx=10, pady=5, sticky=W)
-
-    def testevent(event):
+    #update
+    def updateout(event): # func to update outbox contents and processs txtbox
         print("pressed", event.char)
+        txtboxstring = realtimeChanger(txtbox.get("1.0", "end"), kazalphabet)
+        outbox.delete("1.0", "end")  # if you want to remove the old data
+        outbox.insert(END,txtboxstring)
+        print("txtboxstring contents are:", txtboxstring)
 
-    txtbox.bind("<Key>", testevent)
+    txtbox.bind("<Key>", updateout)# bind all keys to updateout() func
     #misc
-#    callLoop(realKTL, textbox, kazalphabet)
-#    realKTL.after(10, lambda:  outbox.delete(1.0,END))
-#    realKTL.after(10, lambda:  outbox.insert(1.0,callLoop))
-
-
     realKTL.iconbitmap('images/KTL_logo.ico')
     realKTL.resizable(0, 0)
 
 
-def settingsKTL():
+def settingsKTL(): #settings toplevel to show the settings
     settingsKTL = Toplevel(bg = "#f16161")
     settingsKTL.title(' - Settings')
 
@@ -128,7 +122,7 @@ def settingsKTL():
 
     #misc
     settingsKTL.iconbitmap('images/KTL_logo.ico')
-    settingsKTL.resizable(0, 0)
+    settingsKTL.resizable(0, 0)#
 
 
 def manualKTL():
